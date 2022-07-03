77cc211e0cdd5a71263be75b717c24eee9871c78
gunjak <gunjakjaiswal999@gmail.com>
2022-07-03 19:32:45 +0530

    improve

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    try:
-        raise Exception("testing")
-    except Exception as e:
-        print(e)
     return 'This is adult census income prediction'
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run()
     
